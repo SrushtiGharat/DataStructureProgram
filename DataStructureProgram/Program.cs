7bd5be07bf37cbb1178b72c4bc7e2bb8b708ff3d
Srushti <Srushti@DESKTOP-QP0QMA4>
2020-10-21 15:07:09 +0530

    Add project files.

---
+++ DataStructureProgram/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DataStructureProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Data Structure Program");
+        }
+    }
+}
