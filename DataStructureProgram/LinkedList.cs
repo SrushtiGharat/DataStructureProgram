061e1edfa9fc16ca03f0c7cb2859d113200d4b05
Srushti <Srushti@DESKTOP-QP0QMA4>
2020-10-21 15:22:36 +0530

    [Srushti] Add.Create Linked List of 56 30 70

---
+++ DataStructureProgram/LinkedList.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructureProgram
+{
+    class LinkedList
+    {
+        internal Node head;
+         internal void AddNode(int data)
+        {
+            Node node = new Node(data);
+            if(head == null)
+            {
+                this.head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while(temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+        }
+
+        internal void Display()
+        {
+            if(this.head == null)
+            {
+                Console.WriteLine("List is empty");
+                return;
+            }
+            else
+            {
+                Node temp = head;
+                while(temp != null)
+                {
+                    Console.Write(temp.data + " ");
+                    temp = temp.next;
+                }
+            }
+        }
+    }
+}
--- DataStructureProgram/Program.cs
+++ DataStructureProgram/Program.cs
@@ -7,6 +7,14 @@ namespace DataStructureProgram
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome To Data Structure Program");
+            LinkedList list = new LinkedList();
+            list.AddNode(56);
+            list.AddNode(30);
+            list.AddNode(70);
+
+            Console.WriteLine("Linked List :");
+            list.Display();
+
         }
     }
 }
