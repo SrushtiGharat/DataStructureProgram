1124f9c726c4eb367b4b8b3b13cd767701211dde
Srushti <Srushti@DESKTOP-QP0QMA4>
2020-10-22 21:10:47 +0530

    [Srushti] Add.Push elements on the stack

---
+++ StackUsingLinkedList/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using DataStructureProgram;
+namespace StackUsingLinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Stack Program");
+            Stack stack = new Stack();
+            stack.Push(70);
+            stack.Push(30);
+            stack.Push(56);
+
+            Console.WriteLine("Stack :");
+            stack.Display();
+        }
+    }
+}
---
+++ StackUsingLinkedList/Stack.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using DataStructureProgram;
+using System.Text;
+
+namespace StackUsingLinkedList
+{
+    class Stack
+    {
+        Node top;
+        public void Push(int data)
+        {
+            Node node = new Node(data);
+            if (top == null)
+            {
+                this.top = node;
+            }
+            else
+            {
+                Node temp = node;
+                temp.next = top;
+                top = temp;
+            }
+        }
+        public void Display()
+        {
+            if (this.top == null)
+            {
+                Console.WriteLine("Stack is empty");
+                return;
+            }
+            else
+            {
+                Node temp = top;
+                while (temp != null)
+                {
+                    Console.Write(temp.data + " ");
+                    temp = temp.next;
+                }
+            }
+        }
+    }
+}
