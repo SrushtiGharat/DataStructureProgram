fc86a3bec58e75c380e0b67bbdd27249c56c9c4a
Srushti <Srushti@DESKTOP-QP0QMA4>
2020-10-22 22:21:46 +0530

    [Srushti] Add.Implement enqueue operation in queue using linked list

---
+++ QueueUsingLinkedList/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace QueueUsingLinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Queue Program");
+            Queue queue = new Queue();
+            queue.Enqueue(53);
+            queue.Enqueue(30);
+            queue.Enqueue(70);
+
+            Console.WriteLine("Queue :");
+            queue.Display();
+        }
+    }
+}
---
+++ QueueUsingLinkedList/Queue.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DataStructureProgram;
+namespace QueueUsingLinkedList
+{
+    class Queue
+    {
+        Node front;
+        public void Enqueue(int data)
+        {
+            Node node = new Node(data);
+            if (front == null)
+            {
+                this.front = node;
+            }
+            else
+            {
+                Node temp = front;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+        }
+
+        public void Display()
+        {
+            if (this.front == null)
+            {
+                Console.WriteLine("Queue  is empty");
+                return;
+            }
+            else
+            {
+                Node temp = front;
+                while (temp != null)
+                {
+                    Console.Write(temp.data + " ");
+                    temp = temp.next;
+                }
+                Console.WriteLine("\n");
+            }
+        }
+    }
+}
